37b629fcf122a5e0253f7fd70061cd7cb1399138
OusStack <stack01engineer@gmail.com>
2023-02-16 19:23:37 -0800

    always follow guidelines of betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
 
-
 /**
- * main - prints "Programming is like building a multilingual
- * puzzle, followed by a newline."
- * return 0.
+ * main - prints A program that print a line with puts function
+ * Return: 0 (success)
  */
-
 int main(void)
-
 {
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
