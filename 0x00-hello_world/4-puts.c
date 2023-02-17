e44e15b03db27d6f71ddd121b4ee0205084e0f3b
OusStack <stack01engineer@gmail.com>
2023-02-16 19:06:53 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 /**
  * main - prints "Programming is like building a multilingual
  * puzzle, followed by a newline."
@@ -11,5 +12,5 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return(0);
+	return (0);
 }
