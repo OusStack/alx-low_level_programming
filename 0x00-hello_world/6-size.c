04ee60c6a736c3e5cc5f85beee2d8cadab174991
OusStack <stack01engineer@gmail.com>
2023-02-17 09:33:58 -0800

    print f

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
+
 /**
-* main- A program that prints  the size of various computer types
-*
-* Return 0 (Success)
+* main- Entry Point
+* Description: 'A Program that print the size of a file  for various computer program
+* Return: Always 0 (Success)
 */
+
 int main(void)
 {
 	int a;
@@ -17,5 +19,6 @@ printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+
 return (0);
 }
