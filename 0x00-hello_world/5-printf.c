c6e4f07656890ae1557169b4d6ef90bcc17d3b39
OusStack <stack01engineer@gmail.com>
2023-02-17 09:04:35 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - A program that print a line followed by new line using the function printf
- *
- * Return: Always 0 (Success)
- */
+* main - A program that print a line followed by new line using the function printf
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+return (0);
 }
