ee8b5c0f4c14cba358979ecaee5d0704683e9650
ousstack <stack01engineer@gmail.com>
2023-03-10 15:28:43 -0800

    A program that prints its name, followed by a new line.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print the name of progress
+ * @argv: number of argument
+ * @argc: arrays of argument
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
