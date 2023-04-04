18f909d309da653f5f87ef66f1dde5d6bfdfc21e
ousstack <stack01engineer@gmail.com>
2023-04-04 15:44:17 -0700

    updates

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -3,9 +3,7 @@
 /**
  * reverse_listint - Reverses a listint_t linked list.
  * @head: Pointer to the pointer to the head of the list.
- *
  * By Oussama El Attar @OusStack
- *
  * Return: A pointer to the first node of the reversed list.
  */
 listint_t *reverse_listint(listint_t **head)
